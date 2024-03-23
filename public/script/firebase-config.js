e2a31f7c9048d58d1405d77177b83bd16f8a4663
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2024-03-23 18:19:00 +0800

    Add filter drawer
    Add X icon

--- public/script/firebase-config.js
+++ public/script/firebase-config.js
@@ -1,35 +1,24 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: "AIzaSyD36LUSBedozAdZqjGk8qNAaILqnI6SLGc",
-
   authDomain: "apexcode-9d5e1.firebaseapp.com",
-
   projectId: "apexcode-9d5e1",
-
   storageBucket: "apexcode-9d5e1.appspot.com",
-
   messagingSenderId: "1014779145635",
-
   appId: "1:1014779145635:web:fbd980a52c52491099be16",
-
   measurementId: "G-6NWMC4MB57",
 };
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 
 const analytics = getAnalytics(app);
