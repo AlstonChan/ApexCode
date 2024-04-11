46bd96c35638cd50289d3148d6ef13beffbd70cb
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2024-04-11 08:47:42 +0800

    Add support for accordion text

--- components/accordionBar/accordionBar.js
+++ components/accordionBar/accordionBar.js
@@ -30,25 +30,30 @@ accordionBarTemplate.innerHTML = html`<link
   </div>`;
 
 class AccordionBar extends HTMLElement {
-  constructor(_title, _lecture, _isOpen) {
+  /**
+   *
+   * @param {string} _title The title of the accordion bar
+   * @param {string} _lecture The number of lectures in the module, if any
+   * @param {string} _para1 The first paragraph of the content, if any
+   * @param {string} _para2 The second paragraph, if any
+   * @param {string} _para3 The third paragraph, if any
+   */
+  constructor(_title, _lecture, _para1, _para2, _para3) {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(accordionBarTemplate.content.cloneNode(true));
 
     const title = this.getAttribute("title") || _title;
     const lecture = this.getAttribute("lecture") || _lecture;
-    const isOpen = this.getAttribute("open") || _isOpen;
+    const para1 = this.getAttribute("para1") || _para1;
+    const para2 = this.getAttribute("para2") || _para2;
+    const para3 = this.getAttribute("para3") || _para3;
 
     if (title) {
       this.shadowRoot.querySelector("p").textContent = title;
     }
     if (lecture) {
       this.shadowRoot.querySelectorAll("p")[1].textContent = `${lecture} Lecture`;
-    }
-    if (isOpen) {
-      this.shadowRoot.querySelector(".icon").classList.add("open");
-    } else {
-      this.shadowRoot.querySelector(".icon").classList.remove("open");
     }
 
     this.shadowRoot.querySelector("button").addEventListener("click", () => {
@@ -62,11 +67,16 @@ class AccordionBar extends HTMLElement {
         content.classList.add("open");
       }
     });
+
+    if (para1) {
+      this.populatePara(para1, para2, para3);
+    }
   }
 
   /**
    * To populate the content of the accordion bar
    * @param {string[]} lectures
+   * @returns {void}
    */
   populateLecture(lectures) {
     const content = this.shadowRoot.querySelector(".content");
@@ -88,6 +98,33 @@ class AccordionBar extends HTMLElement {
       bar.appendChild(p);
       content.appendChild(bar);
     }
+  }
+
+  /**
+   * To populate the content of the accordion bar
+   * @param {string} para1 The first paragraph
+   * @param {string | undefined} para2 The second paragraph, if any
+   * @param {string | undefined} para3 The third paragraph, if any
+   * @returns {void}
+   */
+  populatePara(para1, para2, para3) {
+    const content = this.shadowRoot.querySelector(".content");
+    const bar = document.createElement("div");
+    bar.classList.add("bar", "para");
+    const p1 = document.createElement("p");
+    bar.appendChild(p1);
+    p1.textContent = para1;
+    if (para2) {
+      const p2 = document.createElement("p");
+      p2.textContent = para2;
+      bar.appendChild(p2);
+    }
+    if (para3) {
+      const p3 = document.createElement("p");
+      p3.textContent = para3;
+      bar.appendChild(p3);
+    }
+    content.appendChild(bar);
   }
 }
 
