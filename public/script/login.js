0cf0b6c1386ee7fa63e10b53528ebd410fae87a7
pekeetong <pek410eetong@gmail.com>
2024-04-27 21:46:28 +0800

    login module done

--- public/script/login.js
+++ public/script/login.js
@@ -22,6 +22,27 @@ const formState = new Proxy(
   }
 );
 
+document.addEventListener("DOMContentLoaded", function() {
+  const signInBtn = document.getElementById("signIn");
+  const signUpBtn = document.getElementById("signUp");
+  const fistForm = document.getElementById("form1");
+  const secondForm = document.getElementById("form2");
+  const container = document.querySelector(".container");
+  signInBtn.addEventListener("click", () => {
+      container.classList.remove("right-panel-active");
+  });
+  
+  signUpBtn.addEventListener("click", () => {
+      container.classList.add("right-panel-active");
+  });
+  
+  fistForm.addEventListener("submit", (e) => e.preventDefault());
+  secondForm.addEventListener("submit", (e) => e.preventDefault());
+  
+  console.log("signInBtn:", signInBtn);
+  console.log("signUpBtn:", signUpBtn);
+});
+
 loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
