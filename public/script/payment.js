4a945d402dbf7c10ba53a18c5844f79ebb4c0048
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2024-05-16 11:31:17 +0800

    Fixed payment page

--- public/script/payment.js
+++ public/script/payment.js
@@ -1,24 +1,10 @@
-var price = document.getElementById("price");
-localStorage.getItem("price"); //follow martin part
-document.write("TOTAL: RM" + price);
+const price = document.querySelectorAll("span[data-price]");
+const audio = document.getElementById("audio");
+const popup = document.getElementById("popup");
+const form = document.getElementById("paymentForm");
 
-var audio = document.getElementById("audio");
-var popup = document.getElementById("popup");
-var div = document.getElementsByClassName("div")
-document.getElementById("pay").addEventListener("click", () => {
-    audio.play(); //need to wait a while
-    popup.style.display = "flex";
-    div.style.opacity = 0;
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  audio.play();
+  popup.classList.remove("hidden");
 });
-
-
-//const dimmedBg = document.createElement("div");
-//dimmedBg.classList.add("hidden");
-//dimmedBg.id = "dimmed-bg";
-//document.querySelector("body").appendChild(dimmedBg);
-//const dimmedBg = document.getElementById("dimmed-bg");
-
-//if(popup.style.display = "flex"){
-//    popup.style.opacity = "1";
-//    div.style.opacity = "0.2";
-//}
